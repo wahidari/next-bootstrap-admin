4990ab72aa5bd850372fb507c5cb9b6597fbd50b
wahidari <wahiid.ari@gmail.com>
2022-01-10 19:14:25 +0700

    add mobile nav using offcanvas

---
+++ components/MobileNav.js
@@ -0,0 +1,129 @@
+import { Offcanvas } from "react-bootstrap";
+import { FaTachometerAlt, FaChartArea, FaTable, FaAtom, FaColumns, FaAngleDown, FaBookOpen, FaTimes } from "react-icons/fa"
+import ActiveLink from "./ActiveLink";
+
+export default function MobileNav({ show, onHide, onClose }) {
+
+  return (
+    <>
+      <style jsx>
+        {`
+          .icon-18 {
+            font-size: 18px;
+          }
+        `}
+      </style>
+      <Offcanvas show={show} onHide={onHide} className="mobile-nav">
+        <Offcanvas.Header className="text-white">
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+          <button className="btn btn-sm" href="#!" onClick={onClose}>
+            <i className="icon-18"><FaTimes className="text-white" /></i>
+          </button>
+        </Offcanvas.Header>
+        <Offcanvas.Body className="p-0">
+          <nav className="sb-sidenav sb-sidenav-dark">
+            <div className="sb-sidenav-menu">
+              <div className="nav">
+                <div className="sb-sidenav-menu-heading">Core</div>
+                <ActiveLink activeClassName="active" href="/">
+                  <a className="nav-link">
+                    <FaTachometerAlt className="me-2" />
+                    Dashboard
+                  </a>
+                </ActiveLink>
+                <div className="sb-sidenav-menu-heading">Interface</div>
+                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                  <FaColumns className="me-2" />
+                  Layouts
+                  <div className="sb-sidenav-collapse-arrow"><FaAngleDown /></div>
+                </a>
+                <div className="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                  <nav className="sb-sidenav-menu-nested nav">
+                    <ActiveLink activeClassName="active" href="/light-sidenav">
+                      <a className="nav-link">
+                        Light Theme
+                      </a>
+                    </ActiveLink>
+                  </nav>
+                </div>
+                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                  <FaBookOpen className="me-2" />
+                  Pages
+                  <div className="sb-sidenav-collapse-arrow"><FaAngleDown /></div>
+                </a>
+                <div className="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                  <nav className="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                    <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                      Authentication
+                      <div className="sb-sidenav-collapse-arrow"><FaAngleDown /></div>
+                    </a>
+                    <div className="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                      <nav className="sb-sidenav-menu-nested nav">
+                        <ActiveLink activeClassName="active" href="/login">
+                          <a className="nav-link">
+                            Login
+                          </a>
+                        </ActiveLink>
+                        <ActiveLink activeClassName="active" href="/register">
+                          <a className="nav-link">
+                            Register
+                          </a>
+                        </ActiveLink>
+                        <ActiveLink activeClassName="active" href="/forgot">
+                          <a className="nav-link">
+                            Forgot Password
+                          </a>
+                        </ActiveLink>
+                      </nav>
+                    </div>
+                    <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                      Error
+                      <div className="sb-sidenav-collapse-arrow"><FaAngleDown /></div>
+                    </a>
+                    <div className="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                      <nav className="sb-sidenav-menu-nested nav">
+                        <ActiveLink activeClassName="active" href="/404">
+                          <a className="nav-link">
+                            404 Page
+                          </a>
+                        </ActiveLink>
+                        <ActiveLink activeClassName="active" href="/500">
+                          <a className="nav-link">
+                            500 Page
+                          </a>
+                        </ActiveLink>
+                      </nav>
+                    </div>
+                  </nav>
+                </div>
+                <div className="sb-sidenav-menu-heading">Addons</div>
+                <ActiveLink activeClassName="active" href="/chart">
+                  <a className="nav-link">
+                    <FaChartArea className="me-2" />
+                    Charts
+                  </a>
+                </ActiveLink>
+                <ActiveLink activeClassName="active" href="/table">
+                  <a className="nav-link">
+                    <FaTable className="me-2" />
+                    Tables
+                  </a>
+                </ActiveLink>
+                <ActiveLink activeClassName="active" href="/component">
+                  <a className="nav-link">
+                    <FaAtom className="me-2" />
+                    Component
+                  </a>
+                </ActiveLink>
+              </div>
+            </div>
+            <div className="sb-sidenav-footer">
+              <div className="small">Logged in as:</div>
+              Admin
+            </div>
+          </nav>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  )
+}
--- components/TopNav.js
+++ components/TopNav.js
@@ -1,8 +1,13 @@
 import Link from "next/link";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { FaBars, FaUserAlt, FaSearch } from "react-icons/fa"
+import MobileNav from "./MobileNav";
 
 export default function TopNav() {
+  const [showMobileNav, setShowMobileNav] = useState(false);
+
+  const handleCloseMobileNav = () => setShowMobileNav(false);
+  const handleShowMobileNav = () => setShowMobileNav(true);
 
   useEffect(() => {
     // Toggle the side navigation
@@ -11,7 +16,6 @@ export default function TopNav() {
       sidebarToggle.addEventListener('click', event => {
         event.preventDefault();
         document.body.classList.toggle('sb-sidenav-toggled');
-        localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
       });
     }
   }, []);
@@ -23,10 +27,19 @@ export default function TopNav() {
         <Link href="/">
           <a className="navbar-brand ps-3">Next Bootstrap Admin</a>
         </Link>
-        {/* <!-- Sidebar Toggle--> */}
-        <button className="btn btn-sm order-1 order-md-0 me-3 me-lg-0 pt-0" id="sidebarToggle" href="#!">
+        {/* <!-- Sidebar Toggle on Large Screen--> */}
+        {/* <!-- Hide on Medium Screen--> */}
+        <button className="d-none d-lg-block btn btn-sm order-1 order-md-0 me-3 me-lg-0 pt-0" id="sidebarToggle" href="#!">
           <i><FaBars /></i>
         </button>
+        {/* <!-- Sidebar Toggle on Small Screen--> */}
+        {/* <!-- Hide on Large Screen--> */}
+        <button className="d-lg-none btn btn-sm order-1 order-lg-0 me-3 me-lg-0 pt-0" href="#!" onClick={handleShowMobileNav}>
+          <i><FaBars className="text-white"/></i>
+        </button>
+
+        <MobileNav show={showMobileNav} onHide={handleCloseMobileNav} onClose={handleCloseMobileNav}/>
+
         {/* <!-- Navbar Search--> */}
         <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
           <div className="input-group">
@@ -35,7 +48,7 @@ export default function TopNav() {
           </div>
         </form>
         {/* <!-- Navbar--> */}
-        <ul className="navbar-nav ms-auto ms-md-0 me-md-3">
+        <ul className="navbar-nav ms-auto ms-md-0 me-lg-3">
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle pt-1" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               <FaUserAlt className="me-1" />
