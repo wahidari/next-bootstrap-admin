c98e1d82f6c116b83c43ec26364b6c9895555ba2
wahidari <wahiid.ari@gmail.com>
2022-02-21 12:35:46 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle")
+    import("bootstrap/dist/js/bootstrap.bundle.min.js")
   }, []);
   
   return <Component {...pageProps} />
